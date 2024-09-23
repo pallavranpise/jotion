06758260f03202439f06bb017b910060bd1a9686
pallavranpise <pallav.ranpise@gmail.com>
2024-09-23 17:40:26 +0530

    editor WIP

--- hooks/use-cover-image.tsx
+++ hooks/use-cover-image.tsx
@@ -1,13 +1,17 @@
 import { create } from "zustand";
 
 type CoverImage = {
+  url?: string;
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
+  onReplace: (url: string) => void;
 };
 
 export const useCoverImage = create<CoverImage>((set) => ({
   isOpen: false,
-  onOpen: () => set({ isOpen: true }),
-  onClose: () => set({ isOpen: false }),
+  url: undefined,
+  onOpen: () => set({ isOpen: true, url: undefined}),
+  onClose: () => set({ isOpen: false, url: undefined }),
+  onReplace: (url: string) => set({ isOpen: true, url }),
 }));
